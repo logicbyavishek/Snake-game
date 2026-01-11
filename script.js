b79a506361f659483d6c74f01c17467c1823ec17
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2026-01-12 03:39:12 +0530

    snake create and appear

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const board = document.querySelector('.board');
-const blockHeight = 80;
-const blockWidth = 80;
+const blockHeight = 50;
+const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth/blockWidth);
 const rows = Math.floor(board.clientHeight/blockHeight);
 
-// for(let i = 0; i < row * col; i++) {
-//     const block = document.createElement('div');
-//     block.classList.add('block');
-//     board.appendChild(block);
-// }
+const blocks = [];
+const snake = [{
+        x:1,
+        y:4,
+},{
+        x:1,
+        y:3,
+},{
+        x:1,
+        y:2,
+    }];
 
+
+// Create grid dynamically and visually represent blocks
 for(let row = 0; row < rows; row++){
     for(let col = 0; col < cols; col++){
         const block = document.createElement('div');
         block.classList.add('block');
         board.appendChild(block);
-        block.innerText=`${row}-${col}`;
+        block.innerText = `${row}-${col}`;
+        blocks[`${row}-${col}`] = block;
     }
 }
+
+function renderSnake(){
+    snake.forEach(segment=>{
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+}
+
+setTimeout(() => {
+    renderSnake();
+}, 300);
