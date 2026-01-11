3e51739647a2d62253b28889112050ebfdecb3af
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2026-01-12 02:55:57 +0530

    2d array visualisation

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const board = document.querySelector('.board');
-const blockHeight = 30;
-const blockWidth = 30;
+const blockHeight = 80;
+const blockWidth = 80;
 
-const col = Math.floor(board.clientWidth/blockWidth);
-const row = Math.floor(board.clientHeight/blockHeight);
+const cols = Math.floor(board.clientWidth/blockWidth);
+const rows = Math.floor(board.clientHeight/blockHeight);
 
-for(let i = 0; i < row * col; i++) {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block);
+// for(let i = 0; i < row * col; i++) {
+//     const block = document.createElement('div');
+//     block.classList.add('block');
+//     board.appendChild(block);
+// }
+
+for(let row = 0; row < rows; row++){
+    for(let col = 0; col < cols; col++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+        block.innerText=`${row}-${col}`;
+    }
 }
